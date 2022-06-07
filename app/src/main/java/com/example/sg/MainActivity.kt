5e4f8d4fdc2beb3c1bdeef4f3e88ff7051cbf139
Dillon410917465 <92067740+Dillon410917465@users.noreply.github.com>
2022-06-07 20:03:18 +0800

    v4

--- app/src/main/java/com/example/sg/MainActivity.kt
+++ app/src/main/java/com/example/sg/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.example.sg
-
-
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,24 +6,19 @@ import android.widget.ImageView
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
-
 @GlideModule
 public final class MyAppGlideModule : AppGlideModule()
-
 class MainActivity : AppCompatActivity() {
-
     lateinit var img : ImageView
     lateinit var game : Game
     var flag:Boolean = false
     lateinit var job : Job
     lateinit var imgAuthor : ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         img = findViewById(R.id.img)
         game = findViewById(R.id.game)
-
         imgAuthor = findViewById(R.id.imgAuthor)
         GlideApp.with(this)
             //.load(R.drawable.earth)
@@ -33,7 +26,6 @@ class MainActivity : AppCompatActivity() {
             .circleCrop()
             .override(800, 600)
             .into(imgAuthor)
-
         img.setOnClickListener({
             if (flag){
                 flag = false
@@ -46,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                 job = GlobalScope.launch(Dispatchers.Main) {
                     while(flag) {
                         delay(10)
+                        game.fly.update()
                         var canvas: Canvas = game.surfaceHolder.lockCanvas()
                         game.drawSomething(canvas)
                         game.surfaceHolder.unlockCanvasAndPost(canvas)
